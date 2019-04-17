42feae8879840807ed1a7cb5b46b47d15a35c6ca
GabrielPilan12 <gabrielcpilan12@hotmail.com>
2019-04-17 02:09:03 -0300

    Implementado a Interface Consultar Fornecedor
    Foram adicionados todas as funcionalidades da tela

--- TG_Compras/src/view/GUI_PesquisarFornecedor.java
+++ TG_Compras/src/view/GUI_PesquisarFornecedor.java
@@ -5,6 +5,23 @@
  */
 package view;
 
+import control.Conexao;
+import control.DaoFornecedor;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.RowFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+import model.Fornecedor;
+import net.proteanit.sql.DbUtils;
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
+
 /**
  *
  * @author M
@@ -14,9 +31,39 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
     /**
      * Creates new form GUI_PesquisarFornecedor
      */
+    //PARA FAZER FUNCIONAR A PESQUISA DINAMICA PRECISA DISSO
+    DefaultTableModel dm=null;
+    
     public GUI_PesquisarFornecedor() {
         initComponents();
+        
         jTableFornecedores.setAutoCreateRowSorter(true);
+        
+        
+        conexao = new Conexao("GABRIEL", "GABRIEL");
+        conexao.setDriver("oracle.jdbc.driver.OracleDriver");
+        conexao.setConnectionString("jdbc:oracle:thin:@localhost:1521:xe");
+        daoFornecedor = new DaoFornecedor(conexao.conectar());
+        
+        
+        //PARA CARREGAR DADOS DO BANCO DE DADOS NA TABELA
+        String sqlquery = "Select CNPJ , NOMEFORNECEDOR , CEP , CIDADE , UF , TELPRINCIPAL , TELSECUNDARIO , EMAIL from tbl_fornecedor";
+        Statement stmt;
+        ResultSet rs;
+        try{
+            stmt = conexao.conectar().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            rs = stmt.executeQuery(sqlquery);
+            jTableFornecedores.setModel(DbUtils.resultSetToTableModel(rs));
+        }catch (SQLException ex){
+            Logger.getLogger(GUI_PesquisarFornecedor.class.getName()).log(Level.SEVERE,null,ex);
+        }    
+        
+        //PARA FAZER FUNCIONAR A PESQUISA DINAMICA PRECISA DISSO
+        dm = (DefaultTableModel) jTableFornecedores.getModel();
+        
+       
+        
+        
     }
 
     /**
@@ -34,17 +81,22 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
         rbPesquisarPorNome = new javax.swing.JRadioButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        txtCNPJ = new javax.swing.JTextField();
-        cmbNomeFornecedor = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableFornecedores = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
         btnCopiarCNPJ = new javax.swing.JButton();
-        btnPesquisarFornecedor = new javax.swing.JButton();
+        ftxtCNPJ = new javax.swing.JFormattedTextField();
+        txtNomeFornecedor = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Pesquisar Fornecedor");
         setAlwaysOnTop(true);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         buttonGroup1.add(rbPesquisaPorCNPJ);
         rbPesquisaPorCNPJ.setText("Pesquisar Fornecedor pelo CNPJ");
@@ -68,9 +120,6 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
 
         jLabel4.setText("Selecione uma opção de pesquisa do fornecedor");
 
-        cmbNomeFornecedor.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        cmbNomeFornecedor.setEnabled(false);
-
         jTableFornecedores.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null},
@@ -87,9 +136,33 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
         jLabel3.setText("Tabela de Fornecedores");
 
         btnCopiarCNPJ.setText("Copiar CNPJ do Fornecedor Selecionado");
+        btnCopiarCNPJ.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCopiarCNPJActionPerformed(evt);
+            }
+        });
 
-        btnPesquisarFornecedor.setText("Pesquisar Fornecedor");
-        btnPesquisarFornecedor.setEnabled(false);
+        try {
+            ftxtCNPJ.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##.###.###/####-##")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+        ftxtCNPJ.setEnabled(false);
+        ftxtCNPJ.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                ftxtCNPJKeyReleased(evt);
+            }
+        });
+
+        txtNomeFornecedor.setEnabled(false);
+        txtNomeFornecedor.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtNomeFornecedorKeyReleased(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        jLabel5.setText("*Atencao a pesquisa feita eh Case Sensitive ou seja, maiusculas e minusculas serao diferenciadas na pesquisa");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -106,23 +179,23 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
                                         .addComponent(jLabel1)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(txtCNPJ, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(btnPesquisarFornecedor))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(ftxtCNPJ, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
                                     .addComponent(rbPesquisaPorCNPJ))
-                                .addGap(18, 18, 18)
+                                .addGap(30, 30, 30)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(rbPesquisarPorNome)
                                     .addGroup(layout.createSequentialGroup()
                                         .addComponent(jLabel2)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(txtNomeFornecedor, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(18, 18, 18)
-                                        .addComponent(cmbNomeFornecedor, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                        .addComponent(jLabel5)))))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(btnCopiarCNPJ)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1127, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
@@ -132,24 +205,23 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel1)
+                        .addComponent(ftxtCNPJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(rbPesquisaPorCNPJ)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(txtCNPJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnPesquisarFornecedor)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(rbPesquisarPorNome)
+                            .addComponent(rbPesquisarPorNome)
+                            .addComponent(rbPesquisaPorCNPJ))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
-                            .addComponent(cmbNomeFornecedor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(txtNomeFornecedor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel5))))
                 .addGap(18, 18, 18)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, 16, Short.MAX_VALUE)
                 .addComponent(btnCopiarCNPJ)
                 .addContainerGap())
         );
@@ -157,27 +229,61 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    //PARA FAZER FUNCIONAR A PESQUISA DINAMICA PRECISA DISSO
+     private void filter(String query){        
+         TableRowSorter<DefaultTableModel> tr=new TableRowSorter<>(dm);
+         jTableFornecedores.setRowSorter(tr);         
+         tr.setRowFilter(RowFilter.regexFilter(query));
+        }
+    
     private void rbPesquisaPorCNPJActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbPesquisaPorCNPJActionPerformed
         if(rbPesquisaPorCNPJ.isSelected()){
-            txtCNPJ.setEnabled(true);
-            btnPesquisarFornecedor.setEnabled(true);
-            cmbNomeFornecedor.setEnabled(false);
+            ftxtCNPJ.setEnabled(true);
+            txtNomeFornecedor.setText("");
+            txtNomeFornecedor.setEnabled(false);
+
         }else{
-            txtCNPJ.setEnabled(false);
-            btnPesquisarFornecedor.setEnabled(false);
+            ftxtCNPJ.setEnabled(false);
+            
         }
     }//GEN-LAST:event_rbPesquisaPorCNPJActionPerformed
 
     private void rbPesquisarPorNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbPesquisarPorNomeActionPerformed
         if(rbPesquisarPorNome.isSelected()){
-            cmbNomeFornecedor.setEnabled(true);
-            txtCNPJ.setEnabled(false);
-            btnPesquisarFornecedor.setEnabled(false);
+            txtNomeFornecedor.setEnabled(true);
+            ftxtCNPJ.setEnabled(false);         
 
         }else{
-            cmbNomeFornecedor.setEnabled(false);
+            txtNomeFornecedor.setEnabled(false);
         }
     }//GEN-LAST:event_rbPesquisarPorNomeActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+    
+    }//GEN-LAST:event_formWindowOpened
+
+    private void ftxtCNPJKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_ftxtCNPJKeyReleased
+        //PARA FAZER FUNCIONAR A PESQUISA DINAMICA PRECISA DISSO
+        String query = ftxtCNPJ.getText().replace(".", "").replace("-", "").replace("/", "").trim();
+        System.out.println(query);
+        filter(query);
+    }//GEN-LAST:event_ftxtCNPJKeyReleased
+
+    private void txtNomeFornecedorKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNomeFornecedorKeyReleased
+        //PARA FAZER FUNCIONAR A PESQUISA DINAMICA PRECISA DISSO
+        String query = txtNomeFornecedor.getText();
+        System.out.println(query);
+        filter(query);
+    }//GEN-LAST:event_txtNomeFornecedorKeyReleased
+
+    private void btnCopiarCNPJActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCopiarCNPJActionPerformed
+        //PARA COPIAR O TEXTO SELECIONADO DA TABELA PARA O BUFFER (CLIPBOARD)
+        String myString = jTableFornecedores.getValueAt(jTableFornecedores.getSelectedRow(), 0).toString();
+        StringSelection stringSelection = new StringSelection(myString);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }//GEN-LAST:event_btnCopiarCNPJActionPerformed
 
     /**
      * @param args the command line arguments
@@ -216,17 +322,22 @@ public class GUI_PesquisarFornecedor extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCopiarCNPJ;
-    private javax.swing.JButton btnPesquisarFornecedor;
     private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JComboBox<String> cmbNomeFornecedor;
+    private javax.swing.JFormattedTextField ftxtCNPJ;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTableFornecedores;
     private javax.swing.JRadioButton rbPesquisaPorCNPJ;
     private javax.swing.JRadioButton rbPesquisarPorNome;
-    private javax.swing.JTextField txtCNPJ;
+    private javax.swing.JTextField txtNomeFornecedor;
     // End of variables declaration//GEN-END:variables
+private Conexao conexao = null;
+private Fornecedor fornecedor = null;
+private DaoFornecedor daoFornecedor = null;
+private List<Fornecedor> listaComboFornecedor = null;
+private Connection conn;
 }
